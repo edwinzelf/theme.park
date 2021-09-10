61293d5fca5f6c2eafe72106b56af85398c359f0
edwinzelf <edwinzelf@me.com>
2021-09-10 13:53:43 +0200

    Update custom_text_header.js

--- CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,12 +6,13 @@
 // You must escape any backward slashes. "\\"
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>.______   .______       __  ____    ____  _______ 
+<div class="custom-text-header"> <pre>
+.______   .______       __  ____    ____  _______ 
 |   _  \  |   _  \     |  | \   \  /   / |   ____|
 |  |_)  | |  |_)  |    |  |  \   \/   /  |  |__   
 |   ___/  |      /     |  |   \      /   |   __|  
 |  |      |  |\  \----.|  |    \    /    |  |____ 
 | _|      | _| `._____||__|     \__/     |_______|
-                                                  </pre> </div>
+</pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
